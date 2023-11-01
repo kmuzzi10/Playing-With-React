a7748c1d4baa9fe9b1a8e4427bec774706b407bf
kmuzzi10 <kmuzammil12546@gmail.com>
2023-11-02 02:35:31 +0500

    react

---
+++ contacts/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+//import statement ends
+
+ReactDOM.render(<App />, document.getElementById("root"));
